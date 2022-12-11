c6ed39577875399e4979553b4b2b80771dea448c
smarttommyau <smarttommyau@gmail.com>
2022-12-11 20:49:47 +0800

    Add ListNode structure

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "leetcode.hpp"
+using namespace std;
+
+int main(){
+  ListNode * node;
+  cin >> node;
+  ListNode o = *node;
+  cout << node->val;
+  cout << o;
+  return 0;
+}
+
