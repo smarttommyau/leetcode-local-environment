38c8129e188fb890ce2ed71fa8de117288331bf4
smarttommyau <smarttommyau@gmail.com>
2022-12-13 00:27:58 +0800

    On work Node

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <type_traits>
+#include "leetcode.hpp"
+
+using namespace std;
+class Solution{
+  public:
+    //method which return thing
+    vector<vector<int>> method1(vector<int> &nums){
+      //do your stuff
+      return vector<vector<int>>();
+    }
+    //method that use the pass in value as resurlt
+    void method2(vector<int> &nums){
+      //do something with nums
+    }
+
+};
+int main(){
+  Solution s;
+  // vector<vector<vector<int>>> nums1,nums2;
+  Node* root;
+  //method1
+  cin >> root;
+  // cout << root->left->left->val;
+  Node out = *root;
+  // cout << out.children[0]->val;
+  cout << out;
+  //method2
+  // cin >> nums2;
+  // s.method2(nums2);
+  // cout << nums2;
+  return 0;
+}
